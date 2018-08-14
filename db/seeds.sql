4166b04c5b32f2ae06d9a58bc0cb852e6ffaf1d5
Syd Vicious <cjschwichtenberg@gmail.com>
2018-08-14 18:33:56 -0400

    adding MYJAWS_BD & Heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,7 @@
+USE o061mve3a0qccy37;
+
+
 INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheese Burger', FALSE);burger_name
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheese Burger', FALSE);
