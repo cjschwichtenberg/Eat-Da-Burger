524a9b3c64f734d76f42f3419799f955151cd5d6
Syd Vicious <cjschwichtenberg@gmail.com>
2018-08-14 15:40:28 -0400

    updating all files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheese Burger', FALSE);
